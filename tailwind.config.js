1d855c18bc28e78a4b365c0de0a30c56e8d30d24
Elmar Jansen <elmar@elmarjansen.nl>
2021-01-06 22:10:05 +0100

    correct link color in prose

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,23 @@ module.exports = {
       center: true,
     },
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'white',
+            a: {
+              'text-decoration': 'none',
+              'font-weight': 'bold',
+              color: 'white',
+              '&:hover': {
+                color: 'gray',
+                'text-decoration': 'underline',
+              },
+            },
+          },
+        },
+      },
+  
       colors: {},
     },
   },
